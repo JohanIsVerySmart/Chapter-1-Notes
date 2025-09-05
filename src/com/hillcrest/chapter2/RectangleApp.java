bc63907bc85d64ddc339208a7a2ff8939fa460e2
Johan Jaison <johan.jsnv2@gmail.com>
2025-09-05 08:56:38 -0600

    9/5 Commit

--- src/com/hillcrest/chapter2/RectangleApp.java
+++ src/com/hillcrest/chapter2/RectangleApp.java
@@ -1,25 +1,25 @@
 package com.hillcrest.chapter2;
 
+import java.awt.*;
+
 /**
  * Class the will build rectngles
  */
 public class RectangleApp {
 
     public static void main(String[] args) {
-        String theDescription = "This application builds and describes rectangles";
-        System.out.println(theDescription);
-        System.out.println(6 + " bars");
-        System.out.printf("%d bars", 6);
-        System.out.println(7);
-        // single line comment
-
-        /*
-        * hey
-        * im a
-        * multi-line
-        * comment
-        * sooo
-        * cooooooool
-         */
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = r1;
+        r1.setSize(10,20);
+        System.out.println("The width of the rectangle is: " + r2.getWidth());
+        Rectangle r3 = new Rectangle(10,20,100,5);
+        r3.translate(5,10);
+        System.out.println(r3.getWidth());
+        System.out.println(r3.getX());
+        Rectangle r4 = new Rectangle();
+        r1 = r3;
+        r2 = r4;
+        r2 = null;
+        System.out.println(r2.getWidth());
     }
 }
