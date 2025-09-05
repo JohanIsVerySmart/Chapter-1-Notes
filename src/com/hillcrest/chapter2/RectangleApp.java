ab255425a8c9abe268f368f38aa4f73d4210b3a8
Johan Jaison <johan.jsnv2@gmail.com>
2025-09-05 07:55:56 -0600

    Chapter 2 inital commit

---
+++ src/com/hillcrest/chapter2/RectangleApp.java
@@ -0,0 +1,25 @@
+package com.hillcrest.chapter2;
+
+/**
+ * Class the will build rectngles
+ */
+public class RectangleApp {
+
+    public static void main(String[] args) {
+        String theDescription = "This application builds and describes rectangles";
+        System.out.println(theDescription);
+        System.out.println(6 + " bars");
+        System.out.printf("%d bars", 6);
+        System.out.println(7);
+        // single line comment
+
+        /*
+        * hey
+        * im a
+        * multi-line
+        * comment
+        * sooo
+        * cooooooool
+         */
+    }
+}
